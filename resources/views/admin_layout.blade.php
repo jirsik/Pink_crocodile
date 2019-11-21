db9efd282d851b2f60d312b366e08f52f50a2457
Jan <jjirsik@gmail.com>
2019-11-21 16:06:42 +0100

    admins temporarry navbar

---
+++ resources/views/admin_layout.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts/app')
+
+@section('content')
+
+@include('inc/navbar')
+@include('inc/messages')
+
+@yield('admin')
+
+@endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,11 @@
 
 @section('content')
 
+<div class="container">
 
     <div id="root"></div>
 
     <script src="{{ mix('js/app.js') }}"></script>
+</div>
  
 @endsection
