9466a6e6a0aabd2d018f999c1e5bb3416f71a905
Jan <jjirsik@gmail.com>
2019-11-22 17:44:52 +0100

    setting laravel schedule

---
+++ app/Console/Commands/CheckForFinishedAuctions.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\Notifications\AuctionWonNotification;
+use App\AuctionItem;
+
+class CheckForFinishedAuctions extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $auctionItems = AuctionItem::where('winner_notified', 0)
+            ->where('ends_at', '<', date("Y-m-d H:i:s", time()) )
+            ->get();
+        if (count($auctionItems) > 0) {
+            foreach ($auctionItems as $auctionItem) {
+                $this->mail_winner($auctionItem);
+            }
+        }
+    }
+
+    public function mail_winner($auctionItem)
+    {
+        //$auctionItem = AuctionItem::with('bids', 'bids.user')->findOrFail($auctionItem_id);
+        $highestBid = null;
+
+        if (count($auctionItem->bids) > 0) {
+            foreach ($auctionItem->bids as $bid) {
+                if ($highestBid ==null || $highestBid->price < $bid->price) {
+                    $highestBid = $bid;
+                }
+            }
+            $user = $highestBid->user;
+            $user->notify(new AuctionWonNotification($auctionItem, $highestBid));
+        }
+
+        $auctionItem->winner_notified = true;
+        $auctionItem->save();
+    }
+}
--- app/Http/Controllers/AuctionController.php
+++ app/Http/Controllers/AuctionController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\FinalRequest;
-use App\Notifications\AuctionWonNotification;
+// use App\Notifications\AuctionWonNotification;
 use App\AuctionItem;
 use App\Item;
 use App\Doner;
@@ -136,39 +136,36 @@ class AuctionController extends Controller
         return redirect('/item/'.$id)->with('success', 'Item Unassigned!');
     }
     
-    public function mail_winner($auctionItem)
-    {
-        //$auctionItem = AuctionItem::with('bids', 'bids.user')->findOrFail($auctionItem_id);
-        $highestBid = null;
+    // public function mail_winner($auctionItem)
+    // {
+    //     //$auctionItem = AuctionItem::with('bids', 'bids.user')->findOrFail($auctionItem_id);
+    //     $highestBid = null;
 
-        if (count($auctionItem->bids) > 0) {
-            foreach ($auctionItem->bids as $bid) {
-                if ($highestBid ==null || $highestBid->price < $bid->price) {
-                    $highestBid = $bid;
-                }
-            }
-            $user = $highestBid->user;
-            $user->notify(new AuctionWonNotification($auctionItem, $highestBid));
-        }
+    //     if (count($auctionItem->bids) > 0) {
+    //         foreach ($auctionItem->bids as $bid) {
+    //             if ($highestBid ==null || $highestBid->price < $bid->price) {
+    //                 $highestBid = $bid;
+    //             }
+    //         }
+    //         $user = $highestBid->user;
+    //         $user->notify(new AuctionWonNotification($auctionItem, $highestBid));
+    //     }
 
-        $auctionItem->winner_notified = true;
-        $auctionItem->save();
-    }
+    //     $auctionItem->winner_notified = true;
+    //     $auctionItem->save();
+    // }
 
-    public function check_for_ending() {
-        //check for passed and not notified auctions
-        $auctionItems = AuctionItem::where('winner_notified', 0)
-            ->where('ends_at', '<', date("Y-m-d H:i:s", time()) )
-            ->get();
-        if (count($auctionItems) > 0) {
-            foreach ($auctionItems as $auctionItem) {
-                $this->mail_winner($auctionItem);
-            }
-        }
+    // public function check_for_ending() {
+    //     //check for passed and not notified auctions
+    //     $auctionItems = AuctionItem::where('winner_notified', 0)
+    //         ->where('ends_at', '<', date("Y-m-d H:i:s", time()) )
+    //         ->get();
+    //     if (count($auctionItems) > 0) {
+    //         foreach ($auctionItems as $auctionItem) {
+    //             $this->mail_winner($auctionItem);
+    //         }
+    //     }
         
-        return 'ok';
-    }
-
-
-
+    //     return 'ok';
+    // }
 }
