7ab95ad80cbd3caa341590a10b02741d36908669
Sam Judge <samuel.judge.92@gmail.com>
2019-11-27 15:01:51 +0100

    mobile sizing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,9 +10,9 @@
     <script src="{{ mix('js/app.js') }}"></script>
     <script>
         // const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-        // const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+        const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
         // document.getElementById('root').style.width = w
-        // document.getElementById('root').style.height = h
+        document.getElementById('root').style.height = h
     </script>
 </div>
  
